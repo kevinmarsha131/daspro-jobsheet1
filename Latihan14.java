8150df0fd40b033905756ab5ef9d00c63c71ad0c
kevinmarsha131 <kevinmarsha131@gmail.com>
2024-10-11 21:13:57 +0700

    kirim ke repository

---
+++ Latihan14.java
@@ -0,0 +1,22 @@
+/** 
+* Latihan14
+*/
+public class  Latihan14 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Kevin Marsha Hafish A");
+        System.out.println("Saya adalah mahasiswa baru jurusan Teknologi Informasi-POLINEMA");
+            System.out.println("--------------------------------------------------------------------------------------");
+            System.out.println("-------------- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA -------------------");
+            System.out.println("--------------------------------------------------------------------------------------");
+            System.out.println("Nama         : Kevin Marsha Hafish A");
+            System.out.println("NIM          : 244107060077");
+            System.out.println("Kelas        : 1F");
+            System.out.println("No Absen     : 14");
+            System.out.println("Prodi        : D-IV SISTEM INFORMASI BISNIS");
+            System.out.println("Alamat       : Kec. Kedungkandang Kel Arjowinangun PERUM Puri Kartika Asri Blok U-5");
+            System.out.println("No HP        : 081945598493");
+            System.out.println("Hobi         : riding");
+
+    }
+}
